f2ebf3a07a8b998aba79fbddd4aa0503d9ce4ca6
Deena1999 <deenadayalan24vvv@gmail.com>
2021-03-06 17:51:55 +0530

    init

---
+++ array.js
@@ -0,0 +1,20 @@
+// []   OR  Array()
+
+let number=[1,2,3,4,5,6]; // Index always starts from 0
+//          0,1,2,3,4,5
+
+console.log(number[1]);
+
+let number1=new Array(1,2,3,4,5,6);
+
+let elements=["JavaScript",25,'c',true];
+
+let languages=['JavaScript','Python','Java', 'C'];
+
+console.log(languages[1]);
+
+console.log(number);
+
+console.log(number1);
+
+console.log(elements);

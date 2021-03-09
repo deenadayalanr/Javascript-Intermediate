35c14c57a3b0b823518059738b63f749528c815d
Deena1999 <deenadayalan24vvv@gmail.com>
2021-03-09 17:54:51 +0530

    basic array operations

---
+++ basic_array_operations.js
@@ -0,0 +1,48 @@
+let number=[1,2,3,4,5,6,7];
+
+console.log(number);
+
+// push Operation
+
+number.push(9);
+console.log(number);
+
+// pop Operation
+
+number.pop();
+console.log(number);
+
+
+//  shift Operation
+
+number.shift();
+console.log(number);
+
+
+
+// unshift Operation
+
+number.unshift(0);
+console.log(number);
+
+
+
+// reverse Operation
+
+number.reverse();
+console.log(number);
+
+
+
+//   indexof Operation
+
+const result=number.indexOf(4);
+console.log(result);
+
+
+
+//  slice Operation
+
+
+console.log(number.slice(1,4));
+console.log(number.slice(1,));

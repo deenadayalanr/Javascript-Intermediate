309b8ea9e10e5cc7536cca185c3ff51f9ba58715
Deena1999 <deenadayalan24vvv@gmail.com>
2021-03-23 17:50:58 +0530

    Functions Basics

---
+++ functions/basic.js
@@ -0,0 +1,33 @@
+
+// FUNCTION DEFINITION
+
+function demo() {
+    console.log("Inside the function demo");
+}
+
+
+//  FUNCTION CALL
+
+demo();
+
+
+
+function add() {
+    return "Function with a return statement";
+}
+
+console.log(add());
+
+const value=add();
+
+console.log(value);
+
+
+
+
+
+
+
+
+
+

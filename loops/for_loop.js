8a90d23025b73aa365251fcae5031864049cbf9c
Deena1999 <deenadayalan24vvv@gmail.com>
2021-04-04 10:59:04 +0530

    Loops Part-1

---
+++ loops/for_loop.js
@@ -0,0 +1,6 @@
+let n=17;
+
+for(let i=1;i<=10;i++)
+{
+    console.log(`${n} X ${i} = ${i*n}`);
+}
---
+++ loops/while_loop.js
@@ -0,0 +1,7 @@
+let n=17,i=1;
+
+while(i<=10)
+{
+    console.log(`${n} X ${i} = ${i*n}`);
+    i++;
+}

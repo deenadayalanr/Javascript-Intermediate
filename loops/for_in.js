9f07f2c5076d18e877a2b85d05c681a385002e6f
Deena1999 <deenadayalan24vvv@gmail.com>
2021-04-22 17:39:11 +0530

    For_of and For_in loop completed

---
+++ loops/for_in.js
@@ -0,0 +1,14 @@
+let courses={
+    node:8.9,
+    express:9.5,
+    python:8.5,
+    java:8.0
+};
+
+for(let j in courses){
+    console.log(j);
+}
+
+for(let p in courses){
+    console.log(courses[p]);
+}
---
+++ loops/for_of.js
@@ -0,0 +1,5 @@
+const values=[5,6,8,78,24,65,68,43];
+
+for(let h of values){
+    console.log(h);
+}
